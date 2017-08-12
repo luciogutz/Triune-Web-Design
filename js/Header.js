8c77ead106a125f1b4d2a807ad0791cb883ce057
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-08-12 17:58:08 -0500

    building stuff

--- js/Header.js
+++ js/Header.js
@@ -2,25 +2,36 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Route, Link, history } from 'react-router-dom'
 
+const images = {
+    menuLogoImg: "https://github.com/luciogutz/Triune-Web-Design/blob/master/assets/Triune-logo.png?raw=true"
+}
 
-const Header = () => (
-  <section>
-    <section className="header_container">
-      <div className="menu-topBar">
-        <p className="menu-topBar-quote">
-          “Recognizing the need is the primary condition for design.” –Charles Eames
-        </p>
-      </div>
-      <div className="menu">
-        <img className="menu-logoImg" src="https://github.com/luciogutz/Triune-Web-Design/blob/master/assets/Triune-logo.png?raw=true" />
-      </div>
-      <div>
-        <h1 className="header_title">
-          “It’s not about ideas. It’s about making ideas happen.” —Scott Belsky
-        </h1>
-      </div>  
-    </section>
-  </section>
-)
+function delayElement (e) {
+
+}
+
+class Header extends React.Component {
+  render() {
+    return(
+      <section>
+        <section className="header_container">
+          <div className="menu-topBar">
+            <p className="menu-topBar-quote">
+              “It’s not about ideas. It’s about making ideas happen.” —Scott Belsky
+            </p>
+          </div>
+          <div className="menu">
+            <img className="menu-logoImg" src={images.menuLogoImg} />
+          </div>
+          <div>
+            <h1 className="header_sloagan fadeInLeft">
+              We make great Wordpress websites.
+            </h1>
+          </div>
+        </section>
+      </section>
+    )
+  }
+}
 
 export default Header
--- js/Home.js
+++ js/Home.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom'
 import { BrowserRouter, Route, Link, history } from 'react-router-dom'
 import Header from './Header'
 
-const Home = () => (
 
+class Home extends React.Component {
+  render() {
+    return(
       <section>
         <Header />
       </section>
-)
+    )
+  }
+}
 
 export default Home
